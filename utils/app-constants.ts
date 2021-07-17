4d1895c6b3864cdb5442dce6a18ab8c85506a1a6
unknown <you@example.com>
2021-07-17 12:17:31 +0530

    Video resource upload and delete API completed

--- utils/app-constants.ts
+++ utils/app-constants.ts
@@ -1,4 +1,6 @@
 import aws from "aws-sdk";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const DB_USER_MODEL = "userModel";
 export const DB_BATCH_MODEL = "batchModel";
@@ -6,9 +8,9 @@ export const DB_TEAM_MODEL = "teamModel";
 export const DB_RESOURCE_MODEL = "resourceModel";
 
 export const S3 = new aws.S3({
-  accessKeyId: "AKIAXCQP2ZQ6ODC2DO6N",
-  secretAccessKey: "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",
-  region: "ap-south-1",
+  accessKeyId: process.env.ACCESS_ID,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: process.env.REGION,
 });
 
 export const BUCKET = "pestomeet-recordings";
