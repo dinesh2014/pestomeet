f66660221cbcc36bc61f52bc8ff98cf06d63feb2
unknown <you@example.com>
2021-07-22 22:30:52 +0530

    Authentication and Authorization added

--- utils/check-token.ts
+++ utils/check-token.ts
@@ -1,4 +1,5 @@
-require('dotenv').config()
+import dotenv from "dotenv";
+dotenv.config();
 import jwt, { Secret } from 'jsonwebtoken';
 import createError from 'http-errors'
 import { message } from "./response-format";
