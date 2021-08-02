65dae29e332ca967e4a2aca8c6119de1022de239
unknown <you@example.com>
2021-08-02 10:44:38 +0530

    Resource Delete Corrected

--- resource/routes/deleteresource-route.ts
+++ resource/routes/deleteresource-route.ts
@@ -2,6 +2,6 @@ import express, { Router } from "express";
 import deleteresourceController from "../controller/deleteresource-controller";
 
 const deleteresourceRouter: Router = express.Router();
-deleteresourceRouter.delete("/", deleteresourceController);
+deleteresourceRouter.delete("/:resourceId", deleteresourceController);
 
 export default deleteresourceRouter;
